13a74b6e9e33e87065093d24bde1b18c66148884
GhazyGyfar <ghazygyfar@gmail.com>
2025-07-19 18:37:09 +0700

    Update contoh2.py

--- contoh2.py
+++ contoh2.py
@@ -133,11 +133,9 @@ st.set_page_config(layout="wide")
 st.title("🧪 Tabel Periodik Unsur Kimia")
 
 # Buat grid 7x18
-grid = [["" for _ in range(18)] for _ in range(7)]
-
-# Masukkan ke dalam grid
+grid = [["" for _ in range(18)] for _ in range(7)]:
 for _, row in df.iterrows():
-    p, g = int(row["Periode"]) - 1, int(row["Golongan"]) - 1
+    p, g = int(row["Periode"])-1, int(row["Golongan"])-1
     grid[p][g] = row
 
 # Inisialisasi klik
